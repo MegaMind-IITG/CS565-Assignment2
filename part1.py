f8fe01cf9b797b67282cba6a53d367df5ba58cf1
desh2608 <r.desh26@gmail.com>
2017-03-07 20:04:52 +0530

    Batch training added in NPLM

--- part1.py
+++ part1.py
@@ -14,19 +14,18 @@ class NPLM(object):
 
 	def __init__(self, dictionary_size, emb_size=50, window_size=5, hidden_layer_size=100, learning_rate=0.01, l2_reg_lambda=0.01):
 
-		self.input  = tf.placeholder(tf.int32, [window_size], name="input")
-		self.output = tf.placeholder(tf.int32, name="output")
-		self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+		self.input  = tf.placeholder(tf.int32, [None,window_size], name="input")
+		self.output = tf.placeholder(tf.int32, [None], name="output")
 		
 		# Initialization
 		self.W_emb =  tf.Variable(tf.random_uniform([dictionary_size, emb_size], -1.0, +1.0))
 		
 		# Embedding layer
-		x_emb = tf.nn.embedding_lookup(self.W_emb, self.input)				
-		y_emb = tf.nn.embedding_lookup(self.W_emb, self.output)
-
-		x_emb = tf.reshape(x_emb,[-1])
-		x_emb_expanded = tf.expand_dims(x_emb,axis=0)
+		x_emb = tf.nn.embedding_lookup(self.W_emb, self.input)
+		x_emb = tf.reshape(x_emb,[-1,window_size*emb_size])
+		
+		y_one_hot = tf.one_hot(self.output,dictionary_size)
+		
 
 		# print (x_emb.get_shape())
 		# print (y_emb.get_shape())
@@ -34,17 +33,15 @@ class NPLM(object):
 		# Fully connetected layer
 		H = tf.Variable(tf.truncated_normal([window_size*emb_size, hidden_layer_size], stddev=0.1), name="H")
 		d = tf.Variable(tf.constant(0.1, shape=[hidden_layer_size]), name="d")
-		hidden_layer_output = tf.tanh(tf.nn.xw_plus_b(x_emb_expanded, H, d))
+		h1 = tf.tanh(tf.nn.xw_plus_b(x_emb, H, d))
 
 		#Regression layer
 		U = tf.Variable(tf.truncated_normal([hidden_layer_size, dictionary_size], stddev=0.1), name="U")
 		b = tf.Variable(tf.constant(0.1, shape=[dictionary_size]), name="b")
 		W = tf.Variable(tf.truncated_normal([window_size*emb_size,dictionary_size],stddev=0.1), name="W")
 
-		reg_output = tf.add(tf.nn.xw_plus_b(hidden_layer_output, U, b),tf.matmul(x_emb_expanded,W))
+		h2 = tf.add(tf.nn.xw_plus_b(h1, U, b),tf.matmul(x_emb,W))
 
-		#Softmax layer
-		output_pred = (tf.nn.softmax(reg_output))
 
 		l2_loss = tf.constant(0.0)
 		l2_loss += tf.nn.l2_loss(H)
@@ -53,29 +50,22 @@ class NPLM(object):
 
 
 		# prediction and loss function
-		self.prediction = tf.argmax(output_pred, axis=0, name="predictions")
-		y_emb_pred = tf.nn.embedding_lookup(self.W_emb, self.prediction)
-
-		self.loss = tf.norm(tf.subtract(y_emb,y_emb_pred)) + l2_loss
+		self.losses = tf.nn.softmax_cross_entropy_with_logits(logits=h2, labels=y_one_hot)
+		self.loss = tf.reduce_mean(self.losses) + l2_reg_lambda*l2_loss
 
 		session_conf = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
 		self.sess = tf.Session(config=session_conf)  
 
-		self.optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-
-		self.grads_and_vars = self.optimizer.compute_gradients(self.loss)
-		self.global_step = tf.Variable(0, name="global_step", trainable=False)
-		self.train_op = self.optimizer.apply_gradients(self.grads_and_vars, global_step=self.global_step)
+		self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(self.loss)
 
 		self.sess.run(tf.global_variables_initializer())
 
 	def train_step(self, in_batch, out_batch):
     		feed_dict = {
 				self.input: in_batch,
-				self.output: out_batch,
-				self.dropout_keep_prob: 0.5,
+				self.output: out_batch
 	    			}
-   		loss, W_e = self.sess.run([self.loss, self.W_emb], feed_dict)
+   		_,loss, W_e = self.sess.run([self.optimizer, self.loss, self.W_emb], feed_dict)
     		# print ("step "+str(step) + " loss "+str(loss))
     		return W_e,loss
 
@@ -90,16 +80,27 @@ with open('./data/train_data.pickle', 'rb') as handle:
 
 
 nplm = NPLM(len(dictionary))
-no_of_epochs=50
+num_epochs = 5
+num_train = len(data) - 4
+batch_size = 5000
+num_batches = num_train//batch_size
+window = 5
 
 W = []
-for j in xrange(no_of_epochs):
-	for i in xrange(len(data)-4):
-		temp_list=[data[i], data[i+1], data[i+2], data[i+3], data[i+4]]
-		tmp_in=np.array(temp_list,dtype=np.int32)
-		tmp_out=data[i+5]
-		W, loss = nplm.train_step(tmp_in,tmp_out)
-		print("Epoch",j+1,"Token",i,"Loss",loss)
 
-print (W[0])
+for j in range(num_epochs):
+	for i in range(num_batches):
+		x_batch = []
+		y_batch = []
+		for k in range(batch_size):
+			temp_list = []
+			for p in range(window):
+				temp_list.append(data[i*batch_size+k+p])
+		
+			x_batch.append(temp_list)	
+			y_batch.append(data[i*batch_size+k+window])
+
+		W, loss = nplm.train_step(x_batch,y_batch)
+		print("Epoch",j+1,"Batch",i+1,"Loss",loss)
+		
 
